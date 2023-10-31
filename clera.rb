6c547ab2a19d377308618849f3df187ec11f65bb
Aditya Dobhada <138972632+Aditya-Dobhada@users.noreply.github.com>
2023-10-31 22:52:04 +0530

    Update clera.rb

--- clera.rb
+++ clera.rb
@@ -14,6 +14,7 @@ class Clera < Formula
     system "pip3 install cython" # install Cython
     system "./configure", *std_configure_args, "--disable-silent-rules"
     system "make", "install" # build and install
+    bin.install "clera"
   end
 
   test do
